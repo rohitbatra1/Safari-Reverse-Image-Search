ba08e237e83f536a6e5be16a553e1908d1b1406e
Rohit Batra <rb4jx@virginia.edu>
2020-05-18 19:55:35 -0400

    handleContextMenu implemented.

--- Safari Reverse Image Search Extension/script.js
+++ Safari Reverse Image Search Extension/script.js
@@ -5,6 +5,19 @@ function setup(){
 }
 
 function handleContextMenu(event) {
+    console.log("handleContextMenu", event);
+    
+//    extract image data then send to ExensionHandler
+    var image = {};
+    
+    if (event.target){
+        image = getImage(event.target);
+    }
+    
+    safari.extension.setContextMenuEventUserInfo(event, image);
+}
+
+function getImage(node){
     
 }
 
